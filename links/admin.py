ce7071183bf4bd90e86f421f0ccf892a13fcbf3d
= <=>
2018-09-26 20:49:08 +0330

    Add Channels, and change image filename of sites

--- links/admin.py
+++ links/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Category, Site
+from .models import (Category, Site, TelegramChannel, SoroushChannel,
+    IGapChannel, GapChannel, EitaaChannel)
 
 
 admin.site.register(Category)
 admin.site.register(Site)
+admin.site.register(TelegramChannel)
+admin.site.register(SoroushChannel)
--- links/models.py
+++ links/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from taggit.managers import TaggableManager
+import os
+from urllib.parse import urlparse
 
+
+def site_image_path(instance, filename):
+    ext = os.path.splitext(filename)[-1] # filename extension
+    ext_domain = urlparse(instance.link).netloc # domain name
+    if ext_domain.startswith('www.'):
+        ext_domain = ext_domain[4:]
+    new_filename = '{}{}'.format(ext_domain, ext) # filename: domain + extension
+    return 'files/images/sites/{}'.format(new_filename)
 
 class Site(models.Model):
     TYPES = (
@@ -14,12 +24,55 @@ class Site(models.Model):
         null=True)
     description = models.TextField()
     created = models.PositiveSmallIntegerField(default=0, blank=True)
-    image = models.ImageField(upload_to='files/images/sites/')
+    image = models.ImageField(upload_to=site_image_path)
     types = models.CharField(max_length=32, choices=TYPES)
     tags = TaggableManager()
 
     def __str__(self):
-        return '{} - ({})'.format(self.name, self.url)
+        return '{} - ({})'.format(self.name, self.link)
+
+
+class Channel(models.Model):
+    TYPES = (
+        ('iran', 'ایرانی'),
+        ('foreign', 'خارجی'),
+    )
+
+    name = models.CharField(max_length=60)
+    channel_id = models.CharField(max_length=32) # channel's id
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL,
+        null=True)
+    description = models.TextField()
+    created = models.PositiveSmallIntegerField(default=0, blank=True)
+    image = models.ImageField(upload_to='files/images/channels/')
+    types = models.CharField(max_length=32, choices=TYPES)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return '{} - ({})'.format(self.name, self.channel_id)
+
+    class Meta:
+        abstract = True
+
+
+class TelegramChannel(Channel):
+    Channel._meta.get_field('image').upload_to += 'telegram'
+
+
+class SoroushChannel(Channel):
+    Channel._meta.get_field('image').upload_to += 'soroush'
+
+
+class EitaaChannel(Channel):
+    Channel._meta.get_field('image').upload_to += 'eitaa'
+
+
+class IGapChannel(Channel):
+    Channel._meta.get_field('image').upload_to += 'igap'
+
+
+class GapChannel(Channel):
+    Channel._meta.get_field('image').upload_to += 'gap'
 
 
 class Category(models.Model):
